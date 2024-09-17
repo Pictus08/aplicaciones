d390799418a9c95d706b171ecbcdd13598c25353
Pablos08 <pabloluisquinones@gmail.com>
2024-09-17 16:23:59 -0300

    CRUD

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -4,7 +4,7 @@ const prisma = new PrismaClient();
 
 export async function GET() {
   try {
-    const users = await prisma.users.findMany();
+    const users = await prisma.user.findMany();
     return new Response(JSON.stringify(users), { status: 200 });
   } catch (error) {
     return new Response(JSON.stringify({ error: "Failed to fetch users" }), {
@@ -24,7 +24,7 @@ export async function POST(request) {
       );
     }
 
-    const newUser = await prisma.users.create({
+    const newUser = await prisma.user.create({
       data: {
         name,
         email,
